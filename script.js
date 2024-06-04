629f1d9b63d12d0582d971e28e85a2951b114fda
carisaelam <carisaelam@gmail.com>
2024-06-04 06:52:30 -0400

    Change squareCount dynamically
    Allows for dynamic change of how the container
    is divided among the squares. Uses a variable
    squareCount to assign the width and height of the
    container. squareCount cannot go over 100.

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
+/*
+TOD0 
+✅ general grid 
+allow for changing grid square count
+allow for different color styling
+*/
+
 const gridContainer = document.querySelector(".grid__container");
 
 function sketch(e) {
   hoveredSquare = e.target;
   console.log(hoveredSquare.id);
-  hoveredSquare.setAttribute('class', 'sketched')
+  hoveredSquare.setAttribute("class", "sketched");
 }
 
-for (let i = 0; i < 16 * 16; i++) {
+let squareCount = 50;
+
+for (let i = 0; i < squareCount * squareCount; i++) {
   const square = document.createElement("div");
   square.id = "square" + (i + 1);
   square.setAttribute("class", "square");
-  square.addEventListener("mouseover", sketch);
 
+  if (squareCount > 100) {
+    squareCount = 100;
+  }
+  let adjustedDimension = 480 / squareCount;
+
+  square.style.width = adjustedDimension + "px";
+  square.style.height = adjustedDimension + "px";
   gridContainer.appendChild(square);
+  console.log(adjustedDimension);
+  square.addEventListener("mouseover", sketch);
 }
